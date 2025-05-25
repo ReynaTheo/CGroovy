8849b4d0a9dfa650379e4369bc9b808b5e39b586
unknown <reynaatheo@gmail.com>
2025-05-25 21:27:15 +0700

    update home img slider

--- js/home.js
+++ js/home.js
@@ -178,3 +178,10 @@ $(document).ready(function() {
     // Initialize the slider
     initSlider();
 });
+
+
+function goToDetail(songID){
+    if(songID === '1'){
+        window.location.href = 'song-detail.html'
+    }
+}
